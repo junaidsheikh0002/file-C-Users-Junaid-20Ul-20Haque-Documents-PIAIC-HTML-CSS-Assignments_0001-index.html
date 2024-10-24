ef540e31dbdffcbecf56f4eaa5d4e51f1dde0a1c
Junaid Ul Haque sheikh <156045075+junaidsheikh0002@users.noreply.github.com>
2024-10-24 10:06:43 +0500

    https://github.com/junaidsheikh0002/file-C-Users-Junaid-20Ul-20Haque-Documents-PIAIC-HTML-CSS-Assignments_0001-index.html/upload
    https://github.com/junaidsheikh0002/file-C-Users-Junaid-20Ul-20Haque-Documents-PIAIC-HTML-CSS-Assignments_0001-index.html/upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// JavaScript for dynamic elements
+
+// Function to show more information in the hero section
+function showMoreInfo() {
+    alert("Explore our expertise in HTML, CSS, and JavaScript. We transform ideas into reality with modern coding practices.");
+}
+
+// Function to show more details in the About section
+function showAboutDetails() {
+    alert("Codex Solutions has been at the forefront of web development, delivering innovative and efficient solutions tailored to your business needs.");
+}
